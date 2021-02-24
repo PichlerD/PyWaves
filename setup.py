08c47b765a9bbbb636c7ff40e58c62ec212c4dbc
ilker <ilker.icyuz@bitpanda.com>
2021-02-24 10:56:17 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(name='PyWaves',
+      version='0.9.0',
+      description='Object-oriented library for the Waves blockchain platform',
+      url='http://github.com/pywaves/pywaves',
+      author='PyWaves Developers',
+      author_email='dev@pywaves.org',
+      license='MIT',
+      packages=find_packages(),
+      keywords = ['waves', 'blockchain', 'analytics'],
+      install_requires=[
+            'base58',
+            'pyblake2',
+            'python-axolotl-curve25519',
+            'requests',
+            'google-api-python-client'
+      ]
+      )
+
+
